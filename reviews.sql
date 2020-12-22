a8dcbe59cc38f1ad0e21feba03ac57b5233e1f2b
Kai Gong <kaigong@Kais-MacBook-Pro.local>
2020-12-21 22:07:48 -0500

    added queries that used to import csv files into mysql database

--- reviews.sql
+++ reviews.sql
@@ -63,15 +63,39 @@ CREATE TABLE `SDC_catwalk`.`characteristic_reviews`
 (`id`)
 );
 
-
 /*
 SAMPLE CODE FOR LOADING LARGE CSV FILES INTO MYSQL DATABASE
+*/
 LOAD DATA LOCAL
 INFILE '/Users/kaigong/Desktop/SDC/characteristic_reviews.csv' INTO TABLE characteristic_reviews FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE  1 ROWS;
 Query OK, 19337415 rows affected
 (1 min 16.50 sec)
-*/
 
+LOAD DATA LOCAL
+INFILE '/clean_data2/reviewscleanup.csv' INTO TABLE reviews FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE  1 ROWS;
 
+/* create indexs for querying indicators to boost the performance */
 CREATE INDEX product_id_index ON reviews (product_id);
 CREATE INDEX review_id_index ON review_photos (review_id);
+CREATE INDEX product_id_index ON characteristics (product_id);
+CREATE INDEX review_id_index ON characteristic_reviews (review_id);
+
+/* remove double quotes from mysql column */
+UPDATE review_photos
+   SET url = TRIM(BOTH
+'"' FROM url)
+
+/* remove double quotes from mysql column database characteristci*/
+UPDATE characteristics
+   SET name = TRIM(BOTH
+'"' FROM name);
+
+/* disable the ONLY_FULL_GROUP_BY */
+SET GLOBAL sql_mode
+=
+(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+
+/* enable load local infile */
+SHOW GLOBAL VARIABLES LIKE 'local_infile';
+SET GLOBAL local_infile
+= 'ON';
